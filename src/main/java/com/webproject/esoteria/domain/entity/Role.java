6f378c2a446e00ba7ab7075d4eb5de803a3c8270
h1ud <hlud@tutamail.com>
2026-04-21 15:39:47 -0500

    add-functions

--- src/main/java/com/webproject/esoteria/domain/entity/Role.java
+++ src/main/java/com/webproject/esoteria/domain/entity/Role.java
@@ -1,5 +1,17 @@
 package com.webproject.esoteria.domain.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+
+@Entity 
 public class Role {
+
+    @Id 
+    @GeneratedValue(strategy=GenerationType.AUTO)
+    private long id;
+    private String role_name;
 
 }
