252dd8cb7e513e2eb2b47026adcab0d02b079a6b
deyadan7 <114596934+deyadan7@users.noreply.github.com>
2026-05-11 09:15:01 -0500

    primer avance Front-end
    ENCARGADOS: Deyanira -- Favian

---
+++ esoterica-main/src/main/java/com/webproject/esoteria/EsoteriaApplication.java
@@ -0,0 +1,13 @@
+package com.webproject.esoteria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsoteriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsoteriaApplication.class, args);
+	}
+
+}
---
+++ esoterica-main/src/test/java/com/webproject/esoteria/EsoteriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webproject.esoteria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsoteriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
